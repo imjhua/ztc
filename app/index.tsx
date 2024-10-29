ef55c2ed89de1e601a3ce888fd36a8e1ca62da3f
bella.v <bella.v@kakaomobility.com>
2024-10-29 09:50:58 +0900

    add

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native'
 import { WebView } from 'react-native-webview'
 
 const URI =
-  'http://idlt.localhost:2222/%ED%94%84%EB%A6%AC%EB%8B%A4%EC%9D%B4%EB%B9%99%EC%9A%A9%EC%96%B4%EC%A7%91'
+  'https://idlt-apps.vercel.app/%ED%94%84%EB%A6%AC%EB%8B%A4%EC%9D%B4%EB%B9%99%EC%9A%A9%EC%96%B4%EC%A7%91'
   
 const Page = () => {
   return (
